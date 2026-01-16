a6a406006d19ed2484896fc1f47b1780e009a61b
Calien Alves de Lima Monteiro <ohyearsonist@gmail.com>
2026-01-16 14:06:21 -0300

    Fix light mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,40 +2,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Númeno RPG',
-			social: [],
-      tableOfContents: {minHeadingLevel: 1, maxHeadingLevel: 3},
-      defaultLocale: "pt-br",
-      favicon: "./public/favicon.svg",
-      logo: {
-        dark: "./src/assets/Logo Númeno.svg",
-        light: "./src/assets/Logo Númeno (light theme).svg",
-        replacesTitle: true,
+  integrations: [
+      starlight({
+          title: 'Númeno RPG',
+          social: [],
+    tableOfContents: {minHeadingLevel: 1, maxHeadingLevel: 3},
+    defaultLocale: "pt-br",
+    favicon: "./public/favicon.svg",
+    logo: {
+      dark: "./src/assets/Logo Númeno.svg",
+      light: "./src/assets/Logo Númeno (light theme).svg",
+      replacesTitle: true,
+    },
+    credits: true,
+    customCss: ["./src/theme.css"],
+          sidebar: [
+      {
+        label: 'Regras',
+        autogenerate: {directory: 'Regras'},
       },
-      credits: true,
-      customCss: ["./src/theme.css"],
-			sidebar: [
-        {
-          label: 'Regras',
-          autogenerate: {directory: 'Regras'},
-        },
-        {
-          label: "Classes",
-          autogenerate: {directory: "Classes"}
-        },
-        {
-          label: "Espécies",
-          autogenerate: {directory: "Espécies"}
-        },
-        {
-          label: "Changelog",
-          autogenerate: {directory: "Changelog"}
-        },
-			],
-		}),
+      {
+        label: "Classes",
+        autogenerate: {directory: "Classes"}
+      },
+      {
+        label: "Espécies",
+        autogenerate: {directory: "Espécies"}
+      },
+      {
+        label: "Changelog",
+        autogenerate: {directory: "Changelog"}
+      },
+          ],
+      }),
 	],
+
+  adapter: vercel(),
 });
