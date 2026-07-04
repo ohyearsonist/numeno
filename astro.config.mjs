457060da3d28e54595ed21e06d40bbd2b7290f4b
Calien Alves de Lima Monteiro <ohyearsonist@gmail.com>
2026-07-04 17:08:52 -0300

    fix: Balance out all damage
    Also pre-updates the changelogs, fixes formatting in multiple files and adds an Armor Class-like system

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,49 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      starlight({
-          title: 'Númeno RPG',
-          social: [],
-    tableOfContents: {minHeadingLevel: 1, maxHeadingLevel: 3},
-    defaultLocale: "pt-br",
-    favicon: "./public/favicon.svg",
-    logo: {
-      dark: "./src/assets/Logo Númeno.svg",
-      light: "./src/assets/Logo Númeno (light theme).svg",
-      replacesTitle: true,
-    },
-    credits: true,
-    customCss: ["./src/theme.css"],
-          sidebar: [
-      {
-        label: 'Regras',
-        autogenerate: {directory: 'Regras'},
+    starlight({
+      title: "Númeno RPG",
+      social: [],
+      tableOfContents: { minHeadingLevel: 1, maxHeadingLevel: 3 },
+      defaultLocale: "pt-br",
+      favicon: "./public/favicon.svg",
+      logo: {
+        dark: "./src/assets/Logo Númeno.svg",
+        light: "./src/assets/Logo Númeno (light theme).svg",
+        replacesTitle: true,
       },
-      {
-        label: "Classes",
-        autogenerate: {directory: "Classes"}
-      },
-      {
-        label: "Espécies",
-        autogenerate: {directory: "Espécies"}
-      },
-      {
-        label: "Equipamentos",
-        autogenerate: {directory: "Equipamentos"}
-      },
-      {
-        label: "Changelog",
-        autogenerate: {directory: "Changelog"}
-      },
-          ],
-      }),
-	],
+      credits: true,
+      customCss: ["./src/theme.css"],
+      sidebar: [
+        {
+          label: "Regras",
+          autogenerate: { directory: "Regras" },
+        },
+        {
+          label: "Classes",
+          autogenerate: { directory: "Classes" },
+        },
+        {
+          label: "Espécies",
+          autogenerate: { directory: "Espécies" },
+        },
+        {
+          label: "Equipamentos",
+          autogenerate: { directory: "Equipamentos" },
+        },
+        {
+          label: "Changelog",
+          autogenerate: { directory: "Changelog" },
+        },
+      ],
+    }),
+  ],
 
   adapter: vercel(),
 });
